bfdf6e34ee7ba65f620d64826782ebcf21b3f23f
Zachary Dean <Zakedoubled@gmail.com>
2022-08-09 16:24:09 -0400

    commit tree df52c6eba3006e69ea8001e27c7458b7867770f4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    render (
+        <button onFocus={()=>console.log('Good!')} onBlur={()=>console.log('Hey! Eyes on me!')}></button>
+    )
+};
+
+export default EyesOnMe;
